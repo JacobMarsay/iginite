cdf6dd9ce8178118e0056365b98735a07997ffee
JacobMarsay <jmmarsay@outlook.com>
2022-09-09 14:35:29 +0100

    API key added

---
+++ ignite-site/src/app/games/gamesSlice.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const base_url = `https://api.rawg.io/api/`;
+
+const getDate = () => {
+  return new Date().toISOString().slice(0, 10);
+};
+
+const lastYear = Date().getFullYear() - 1;
+const currentYear = new Date().getFullYear();
+const nextYear = new Date().getFullYear() + 1;
+const currentDate = getDate();
+console.log(lastYear);
+console.log(currentYear);
+console.log(nextYear);
+console.log(currentDate);
+
+const popular_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
